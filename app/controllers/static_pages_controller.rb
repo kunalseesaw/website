4aab0976f68ecd527ff7832555d9932fded1bea7
Faye Cheng <fayehcheng@gmail.com>
2016-07-30 21:25:40 -0700

    Adding website to new git repo

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,26 @@
+class StaticPagesController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def resume
+  end
+
+  def leadership
+  end
+
+  def projects
+  end
+
+  def designs
+  end
+
+  def publications
+  end
+
+  def contact
+  end
+  
+end
